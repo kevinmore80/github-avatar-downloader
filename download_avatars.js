4a1d8669b3f76afb709b917c4073b0c12a8d2362
kevinmore80 <kevinmore@outlook.com>
2017-09-26 02:58:01 +0000

    Implement getRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,28 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+console.log('Welcome to the GitHub Avatar Downloader');
+
+var GITHUB_USER = "kevinmore80";
+var GITHUB_TOKEN = "af0cbb963d0b633bee35f463f49c076f599ba20a";
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request( {
+    headers: {
+      'User-Agent': "GitHub Avatar Downloader - Student Project",
+    },
+    url: requestURL
+  }, function(err, response, body) {
+    if (err) throw err;
+    var temp = JSON.parse(body);
+    cb(err,temp);
+  });
 }
+
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  result.forEach(function(element) {
+    console.log(element['avatar_url']);
+  });
 });
