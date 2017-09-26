b6f9fcd9544f48321040841b6d05f576825b6806
kevinmore80 <kevinmore@outlook.com>
2017-09-26 03:15:34 +0000

    Added required CLI arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,4 @@
+var args = process.argv.slice(2);
 var request = require('request');
 var fs = require('fs');
 
@@ -7,7 +8,14 @@ var GITHUB_USER = "kevinmore80";
 var GITHUB_TOKEN = "af0cbb963d0b633bee35f463f49c076f599ba20a";
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
+  if(!repoOwner || !repoName) {
+    console.log ("Please enter valid values for both arguments");
+    return;
+  } else {
+
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
   request( {
     headers: {
       'User-Agent': "GitHub Avatar Downloader - Student Project",
@@ -17,19 +25,19 @@ function getRepoContributors(repoOwner, repoName, cb) {
     if (err) throw err;
     var temp = JSON.parse(body);
     cb(err,temp);
-  });
+    });
+  }
 }
 
 function downloadImageByURL(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath).on('close', function() {
     console.log("DONE");
   }));
- }
+}
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(args[0], args[1], function(err, result) {
   console.log("Errors:", err);
   result.forEach(function(element) {
     downloadImageByURL(element['avatar_url'], "./avatars/" + element['login'] + ".jpg");
-    console.log(element['avatar_url']);
   });
 });
